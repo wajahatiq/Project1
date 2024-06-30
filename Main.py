54fbbf76750f4353b244163b3a832325d62cd2e0
wajahatiq <wajahatv@gmail.com>
2024-06-30 13:41:08 -0700

    6th commit/created (View Total Sales Amount) selection

--- Main.py
+++ Main.py
@@ -11,7 +11,6 @@ def load_inventory():
 load_inventory()
 
 
-
 def add_item (item_name, Item_count, Item_price):
     if item_name not in inventory:
         inventory[item_name] = {'count':Item_count, 'price':Item_price}
@@ -20,7 +19,6 @@ def add_item (item_name, Item_count, Item_price):
         print(inventory)
     else:
         print(f'The {item_name} is already listed in inventory, please update item to make changes.')
-
 
 
 def buy_item (item_name, Item_count):
@@ -48,12 +46,14 @@ def change_price(item_name, item_price):
     else:
         print(f'{item_name} is not present in inventory.')
 
+
 def update_inventory (item_name, Item_count):
     if item_name in inventory:
         inventory[item_name]['count'] = Item_count
         save_inventory()
         print()
         print(f'{item_name} quantity has been updated to {Item_count} units.')
+
 
 def display_inventory ():
     print()
@@ -68,24 +68,22 @@ def display_inventory ():
     print("=" * 40)
 
 
-
-
-
 def save_inventory():
     with open ('a.json', 'w') as f:
         json.dump(inventory, f, indent=4)
-
 
 
 def main ():
     while True:
         print("\n===== Inventory Management System =====")
         print()
-        print('1. Add new items')
-        print('2. Buy items')
-        print('3. Change item price')
-        print('4. Change item quantity')
-        print('5. Display inventory')
+        print('1. Add New Items')
+        print('2. Buy Items')
+        print('3. Change Item Price')
+        print('4. Change Item Quantity')
+        print('5. Display Inventory')
+        print('6. View Total Sales Amount')
+        print('7. Exit')
 
 
         choice = int(input('Please enter your choice:'))
@@ -94,19 +92,26 @@ def main ():
             Item_count = int(input('Please enter number of item:'))
             Item_price= float(input('Please enter price of item:'))
             add_item(item_name, Item_count, Item_price)
-        if choice == 2:
+        elif choice == 2:
             item_name = input('Please enter item name:')
             Item_count = int(input('Please enter number of item:'))
             buy_item (item_name, Item_count)
-        if choice == 3:
+        elif choice == 3:
             item_name = input('Please enter item name:')
             Item_price= float(input('Please enter new price of item:'))
             change_price (item_name, Item_price)    
-        if choice == 4:
+        elif choice == 4:
             item_name = input('Please enter item name:')
             Item_count = int(input('Please enter the new quantity of the item:'))
             update_inventory (item_name, Item_count)
-        if choice == 5:
+        elif choice == 5:
             display_inventory()
+        elif choice == 6:
+            print(f"Total Sales Amount is: PKR{total_sales}.")  
+        elif choice == 7:
+            print("Exiting programme.")
+            break
+        else:
+            print("Invalid choice. Please enter a number between 1 to 7.")     
 
 main()
