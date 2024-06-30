d6d67f34a838ffb56b1a9b1e15ae0bd817386c36
wajahatiq <wajahatv@gmail.com>
2024-06-30 13:22:46 -0700

    Second commit/Created but items method

--- Main.py
+++ Main.py
@@ -4,14 +4,42 @@ total_sales = 0
 inventory = {}
 
 
+def load_inventory():
+    global inventory
+    with open('a.json', 'r') as f:
+            inventory = json.load(f)
+load_inventory()
+
+
+
 def add_item (item_name, Item_count, Item_price):
     if item_name not in inventory:
-        inventory[item_name] = {'Count':Item_count, 'Price':Item_price}
-        print(f'{item_name} added to inventory')
-        print(inventory)
+        inventory[item_name] = {'count':Item_count, 'price':Item_price}
         save_inventory()
+        print(f'{item_name} added to inventory.')
+        print(inventory)
     else:
         print(f'The {item_name} is already listed in inventory, please update item to make changes.')
+
+
+
+def buy_item (item_name, Item_count):
+    global total_sales
+    if item_name in inventory and inventory[item_name]['count']>=Item_count:
+         inventory[item_name]['count'] -= Item_count
+         sale = Item_count * inventory[item_name]['price']
+         total_sales += sale
+         save_inventory()
+         print(f'You have bought {Item_count}{item_name}. Total price is: PKR{sale}.')
+         print('Remaining items in inventory are:')
+         print(inventory)
+    elif item_name in inventory:
+        print(f'Entered quantity is not present. Available: {inventory[item_name]['count']}.')
+    else:
+        print(f'{item_name} is not present in inventory.')
+
+
+
 
 
 def save_inventory():
@@ -23,6 +51,7 @@ def save_inventory():
 def main ():
     while True:
         print('1. Add new items')
+        print('2. Buy items')
 
         choice = int(input('Please enter your choice:'))
         if choice == 1:
@@ -30,5 +59,10 @@ def main ():
             Item_count = int(input('Please enter number of item:'))
             Item_price= float(input('Please enter price of item:'))
             add_item(item_name, Item_count, Item_price)
+        if choice == 2:
+            item_name = input('Please enter item name:')
+            Item_count = int(input('Please enter number of item:'))
+            buy_item (item_name, Item_count)
+
 
 main()
