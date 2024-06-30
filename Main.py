9e555a2dcf7d9fe9327d9ea6fd201b8d9c618b97
wajahatiq <wajahatv@gmail.com>
2024-06-30 13:37:04 -0700

    5th commit/created display_inventory

--- Main.py
+++ Main.py
@@ -55,6 +55,18 @@ def update_inventory (item_name, Item_count):
         print()
         print(f'{item_name} quantity has been updated to {Item_count} units.')
 
+def display_inventory ():
+    print()
+    print("\n===== Current Inventory =====")
+    print('{:<20}{:<10}{:<10}'.format('item Name','Units','Price'))
+    print('='*40)
+    for x, y in inventory.items():
+        item_name = x
+        units = y['count']
+        price = y['price']
+        print("{:<20} {:<8} PKR{:<8}".format(item_name, units, price))
+    print("=" * 40)
+
 
 
 
@@ -67,10 +79,13 @@ def save_inventory():
 
 def main ():
     while True:
+        print("\n===== Inventory Management System =====")
+        print()
         print('1. Add new items')
         print('2. Buy items')
         print('3. Change item price')
         print('4. Change item quantity')
+        print('5. Display inventory')
 
 
         choice = int(input('Please enter your choice:'))
@@ -91,5 +106,7 @@ def main ():
             item_name = input('Please enter item name:')
             Item_count = int(input('Please enter the new quantity of the item:'))
             update_inventory (item_name, Item_count)
+        if choice == 5:
+            display_inventory()
 
 main()
